e4938a664b146ba617cbe4d4e0679b2554e2b50c
Adam NAILI <adam.naili.pro@gmail.com>
2024-06-03 20:22:13 +0200

    feat: limiting the bounds of the camera

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import "./App.scss";
 import { Environment, Helper, OrbitControls } from "@react-three/drei";
 import React from "react";
 import { Room } from "./Room";
-import { PointLightHelper } from "three";
-import { Bloom, EffectComposer, N8AO } from "@react-three/postprocessing";
+import { PointLightHelper, Vector3 } from "three";
+import { EffectComposer, N8AO } from "@react-three/postprocessing";
 import { useSnapshot } from "valtio";
 import { state } from "./store";
 
@@ -14,7 +14,7 @@ const Lights = () => {
       <Helper type={PointLightHelper}/>
     </pointLight>
 
-    <pointLight position={[-2, 4.4, 2]} intensity={10} >
+    <pointLight position={[-2, 4.4, 2]} intensity={10}>
       <Helper type={PointLightHelper}/>
     </pointLight>
     <pointLight position={[3.7, 4.4, 2]} intensity={10}>
@@ -36,20 +36,27 @@ const Scene = () => {
   </>
 }
 
+const CameraControls = () => {
+  const snap = useSnapshot(state);
+
+  const three = useThree();
+  return <OrbitControls enabled={!snap.pivotDragged} onChange={(e) => {
+    three.camera.position.clamp(new Vector3(-5, 1, -7), new Vector3(7, 5, 7))
+  }}/>
+}
+
 
 function App() {
 
-  const snap = useSnapshot(state);
   return (
     <>
-    <Canvas camera={{position: [0,1,5]}}>
+      <Canvas camera={{position: [0, 1, 5]}}>
         <Scene/>
-        <OrbitControls enabled={!state.pivotDragged} maxZoom={1} minZoom={0.1}/>
+        <CameraControls/>
         <Environment preset={"night"} background={true}/>
-      <EffectComposer>
-        <N8AO halfRes/>
-
-      </EffectComposer>
+        <EffectComposer>
+          <N8AO halfRes/>
+        </EffectComposer>
       </Canvas>
     </>
   );
